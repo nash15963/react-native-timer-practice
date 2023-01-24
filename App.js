ff5228437f923b278445172f62786ea291b34f1f
“nash1124” <nash1596315963@gmail.com>
2023-01-24 21:33:11 +0800

    timer commit and pre upload ios

--- App.js
+++ App.js
@@ -1,10 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View ,Pressable ,TouchableHighlight } from 'react-native';
+
+
 
 export default function App() {
+
+  const [second, setSecond] = useState(0)
+  const [timeTurn, setTimeTurn] = useState(false)
+
+  useEffect(()=>{
+    if(!timeTurn) return ;
+
+    let timer = setInterval(()=>{
+      setSecond(prev => prev+1)
+    } ,1000)
+
+    return ()=>clearInterval(timer)
+    
+  },[timeTurn])
+
+  const handleStart =()=>{
+    !timeTurn ? setTimeTurn(true) : setTimeTurn(false)
+
+  }
+  const handleRefesh =()=>{
+    if(!timeTurn){
+      setSecond(0)
+    }
+  }
+
+    const checkTwoDigits = (t) => {
+    if (t < 10) {
+      return "0" + t;
+    }
+    return t;
+  };
+
+  const readTime =(t)=>{
+    const min = Math.floor(t/60)
+    const sec = t - (min*60)
+    
+    return `${checkTwoDigits(min)} : ${checkTwoDigits(sec)}`
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View>
+        <Text style={styles.timeNumber}>{readTime(second)}</Text>
+      </View>
+      
+      <View style={styles.parentButton}>
+      <TouchableHighlight style={styles.Button} onPress={handleStart}>
+        <Text style={styles.text}>START/STOP</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight style={styles.Button} onPress={handleRefesh}>
+        <Text style={styles.text}>REFRESH</Text>
+      </TouchableHighlight>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +69,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timeNumber : {
+    color : 'white' ,
+    fontSize : 100
+  } ,
+  parentButton:{
+    flexDirection: 'row' 
+  } ,
+  Button :{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'white',
+    margin : 20
+  } ,
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
   },
 });
